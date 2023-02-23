efcf829504a0f7afe010165901b55178cc3b7cb1
KhantZawMinnOo <32001077+KhantZawMinnOo@users.noreply.github.com>
2023-02-23 13:33:29 +0630

    Create kup.js
    initial commit

---
+++ kup.js
@@ -0,0 +1,9 @@
+{   
+        "appName": "test", 
+        "versionCode": "2", 
+        "versionName": "1.0", 
+        "apkUrl": "https://github.com/KhantZawMinnOo/Raw/blob/main/%F0%9D%90%BE%F0%9D%91%A2%F0%9D%91%92%F0%9D%91%98%F0%9D%91%A2%20%F0%9D%91%80%F0%9D%91%A2%F0%9D%91%A0%F0%9D%91%96%F0%9D%91%90%20%F0%9D%91%86%F0%9D%92%89%F0%9D%91%8E%F0%9D%91%98%F0%9D%91%92%20%F0%9D%93%95%F0%9D%93%AA%F0%9D%93%B6%F0%9D%93%B2%F0%9D%93%B5%F0%9D%94%82_1.0.apk?raw=true", 
+        "changeLog":"1.Bug fix;\n2.UI update.", 
+        "updateTips": "upd" ,
+        "forceUpgrade": false
+    }
